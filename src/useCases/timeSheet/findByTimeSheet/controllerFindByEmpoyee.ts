8383c711f85dcef47ff7a7751b133cf53878aa16
willian <willianksouza94@gmail.com>
2024-03-14 11:46:55 -0300

    mudança de como o controller timesheet entrega os cookies

--- src/useCases/timeSheet/findByTimeSheet/controllerFindByEmpoyee.ts
+++ src/useCases/timeSheet/findByTimeSheet/controllerFindByEmpoyee.ts
@@ -1,17 +1,28 @@
-import { Request, Response } from "express";
-import { FindEmployeeTimeSheetService } from "./findEmployeeTimeSheetService";
+import { Request, Response } from 'express';
+import { FindEmployeeTimeSheetService } from './findEmployeeTimeSheetService';
 
 export class ControllerFindByEmployeeTimeSheet {
-  constructor(private findEmployeeTimeSheetService: FindEmployeeTimeSheetService) { }
+  constructor(
+    private findEmployeeTimeSheetService: FindEmployeeTimeSheetService,
+  ) {}
   async findTimeSheet(req: Request, res: Response) {
     if (!req.cookies.securityData) {
       return res.status(500).json({ data: 'sem securityData' });
     }
-    const { time_sheet_id } = req.cookies.securityData
+
+    const { time_sheet_id } = req.cookies.securityData;
 
     try {
-      const timeSheetEmployee = await this.findEmployeeTimeSheetService.execute(time_sheet_id);
-      return res.status(200).json({ timeSheetEmployee });
+      const timeSheetEmployee =
+        await this.findEmployeeTimeSheetService.execute(time_sheet_id);
+      return res
+        .status(200)
+        .cookie('securityData', req.cookies.securityData, {
+          httpOnly: true,
+          secure: true,
+          sameSite: 'none',
+        })
+        .json({ timeSheetEmployee });
     } catch (error) {
       return res.status(error.statusCode).json({ data: error.message });
     }
